f849c9baa9a83e64f3b70251a73023114f70fcdb
bscaspar <bradyscaspar@outlook.com>
2018-11-05 11:21:12 -0800

    update login flow to redirect user if email already exists

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
 import { Auth } from 'aws-amplify';
 
 import './Login.css'
